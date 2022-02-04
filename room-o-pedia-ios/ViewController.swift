1185a0ae8be9504c75ac2a613e78b1f8f546cfb0
angiexyang <axyang@brynmawr.edu>
2022-02-04 17:18:52 -0500

    Initial Commit

---
+++ room-o-pedia-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  room-o-pedia-ios
+//
+//  Created by Angie X Yang on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ room-o-pedia-iosUITests/room_o_pedia_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  room_o_pedia_iosUITestsLaunchTests.swift
+//  room-o-pedia-iosUITests
+//
+//  Created by Angie X Yang on 2/4/22.
+//
+
+import XCTest
+
+class room_o_pedia_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
