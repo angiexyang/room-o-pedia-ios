cd18e60ca3433812522768dd763b759910fb67bd
Rachel Lee <rachel.l9109@gmail.com>
2022-04-02 19:16:00 -0400

    photo slider worked :>

---
+++ room-o-pedia-ios/PhotoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoCollectionViewCell.swift
+//  room-o-pedia-ios
+//
+//  Created by Rachel H Lee on 4/2/22.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var roomPhoto: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- room-o-pedia-ios/ViewRoomViewController.swift
+++ room-o-pedia-ios/ViewRoomViewController.swift
@@ -18,98 +18,109 @@ class ViewRoomViewController: UIViewController, UICollectionViewDelegate, UIColl
     var photoURLArray = [String]()
     
     var tagCount = 0
+    var photoCount = 0
    
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     //number of rows
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        var tagTotal = 3 +  room.features.storage.count + room.features.window_direction.count
-        if (!room.features.other.isEmpty && room.features.other[0] != ""){
-            tagTotal = tagTotal + room.features.other.count
+        
+        if collectionView == self.tagCollectionView {
+            var tagTotal = 3 +  room.features.storage.count + room.features.window_direction.count
+            if (!room.features.other.isEmpty && room.features.other[0] != ""){
+                tagTotal = tagTotal + room.features.other.count
+            }
+            return tagTotal
+        } else {
+            print("number of photos \(photoURLArray.count)")
+            return photoURLArray.count
         }
-        return tagTotal
     }
     
     //customize what is displayed inside cell
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "featureTag", for: indexPath) as! TagCollectionViewCell
-        cell.roomTagLabel.font = UIFont.systemFont(ofSize: 11)
-        self.currFeatures = room.features
+        if collectionView == self.tagCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "featureTag", for: indexPath) as! TagCollectionViewCell
+            cell.roomTagLabel.font = UIFont.systemFont(ofSize: 11)
+            self.currFeatures = room.features
         
-        //make feature array with format feature-value to match filters in view controller
-        let floorFeature = "floor-"+self.currFeatures.floor
-        let coolingFeature = "cooling_system-"+self.currFeatures.cooling_system
-        let flooringFeature = "flooring-"+self.currFeatures.flooring
+            //make feature array with format feature-value to match filters in view controller
+            let floorFeature = "floor-"+self.currFeatures.floor
+            let coolingFeature = "cooling_system-"+self.currFeatures.cooling_system
+            let flooringFeature = "flooring-"+self.currFeatures.flooring
         
-        featureArray = [floorFeature, coolingFeature, flooringFeature]
+            featureArray = [floorFeature, coolingFeature, flooringFeature]
 
-        for s in self.currFeatures.storage{
-            featureArray.append("storage-"+s)
-        }
-        
-        for w in self.currFeatures.window_direction{
-            featureArray.append("window_direction-"+w)
-        }
-        
-        for o in self.currFeatures.other{
-            if (o != ""){
-                featureArray.append("other-"+o)
+            for s in self.currFeatures.storage{
+                featureArray.append("storage-"+s)
             }
-        }
         
-        //put in tag labels for each cell, reformat string
-        if tagCount<featureArray.count{
-            let currTag = featureArray[tagCount]
-            let dashIndex = currTag.firstIndex(of: "-")
-            let stringCut = currTag.index(after: dashIndex!)
+            for w in self.currFeatures.window_direction{
+                featureArray.append("window_direction-"+w)
+            }
+        
+            for o in self.currFeatures.other{
+                if (o != ""){
+                    featureArray.append("other-"+o)
+                }
+            }
+        
+            //put in tag labels for each cell, reformat string
+            if tagCount<featureArray.count{
+                let currTag = featureArray[tagCount]
+                let dashIndex = currTag.firstIndex(of: "-")
+                let stringCut = currTag.index(after: dashIndex!)
             
-            //if feature is window direction, add "windows" after direction on the tag
-            let currFeature = String(currTag[...currTag.index(before: dashIndex!)])
-            if (currFeature == "window_direction"){
-                cell.roomTagLabel.text = String(currTag[stringCut...])+" windows"
+                //if feature is window direction, add "windows" after direction on the tag
+                let currFeature = String(currTag[...currTag.index(before: dashIndex!)])
+                if (currFeature == "window_direction"){
+                    cell.roomTagLabel.text =    String(currTag[stringCut...])+" windows"
+                } else {
+                    cell.roomTagLabel.text = String(currTag[stringCut...])
+                }
             }
-            else{
-                cell.roomTagLabel.text = String(currTag[stringCut...])}
+            tagCount+=1
+            return cell
         }
-        tagCount+=1
-        return cell
+        else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCollectionViewCell
+            if photoCount < photoURLArray.count {
+                let currPhoto = photoURLArray[photoCount]
+                cell.roomPhoto.loadFrom(URLAddress: currPhoto)
+                cell.clipsToBounds = true
+                //cell.roomPhoto.image = UIImage(named: "owl")
+                cell.roomPhoto.contentMode = .scaleAspectFit
+                //cell.frame = CGRect(x: 0, y: 0, width: self.photoCollectionView.frame.width/2, height: self.photoCollectionView.frame.height/2)
+            }
+            photoCount+=1
+            return cell
+        }
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == self.photoCollectionView {
+            return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        } else {
+            return CGSize(width: 120, height: 44)
+        }
+    }
 
     @IBOutlet weak var tagCollectionView: UICollectionView!
+    //@IBOutlet weak var roomImageView: UIImageView!
     
-    @IBOutlet weak var roomImageView: UIImageView!
-    
-    /* TESTING SCROLL VIEW */
-    /*
-     
-    var imageScrollView: UIScrollView!
-    var roomImages = [UIImageView]()
-    
-     scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
-         scrollView.backgroundColor = .systemTeal
-         // Set the contentSize to 100 times the height of the phone's screen so that
-         scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: UIScreen.main.bounds.height*100)
-         view.addSubview(scrollView)
-     
-     for i in 0...100 {
-           images.append(UIImageView(image: UIImage(systemName: "person.3.fill")))
-           images[i].frame = CGRect(x: 0, y: UIScreen.main.bounds.height*CGFloat(i), width: view.frame.width, height: view.frame.height)
-           images[i].contentMode = .scaleAspectFit
-           scrollView.addSubview(images[i])
-         }
-     
-    */
-    
+
     @IBOutlet weak var dormRoomLabel: UILabel!
     
     //when tag is clicked, perform unwind segue back to view controller
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
-        print("indexpath\t", indexPath)
-        print("row\t", indexPath.row)
-        //pass through feature selected as an array of 1 string
-        performSegue(withIdentifier: "tagClickSegue", sender: [featureArray[indexPath.row]])
-        
-    
+        if collectionView == self.tagCollectionView {
+            print("indexpath\t", indexPath)
+            print("row\t", indexPath.row)
+            //pass through feature selected as an array of 1 string
+            performSegue(withIdentifier: "tagClickSegue", sender: [featureArray[indexPath.row]])
+        }
 }
     
     //prepare for unwind segue back to view controller
@@ -118,7 +129,15 @@ class ViewRoomViewController: UIViewController, UICollectionViewDelegate, UIColl
         vc.currentFilters = (sender as? [String])!
     }
   
-   
+    //------page controller func--------
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
+    override func viewDidLayoutSubviews() {
+        pageControl.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+    }
+    //---------------
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -127,11 +146,17 @@ class ViewRoomViewController: UIViewController, UICollectionViewDelegate, UIColl
         
         // GET PHOTOURLS
         self.photoURLArray = room.photoURL
-        let currURL = photoURLArray[0] // GET FIRST FOR TESTER
+        self.pageControl.numberOfPages = photoURLArray.count
+        //let currURL = photoURLArray[0] // GET FIRST FOR TESTER
         
-        roomImageView.loadFrom(URLAddress: currURL)
-       // tagCollectionView.delegate = self
+        //roomImageView.loadFrom(URLAddress: currURL)
+        tagCollectionView.delegate = self
         tagCollectionView.dataSource = self
+        photoCollectionView.dataSource = self
+        photoCollectionView.delegate = self
+        
+        photoCollectionView.isPagingEnabled = true
+        photoCollectionView.showsHorizontalScrollIndicator = false
         // Do any additional setup after loading the view.
     }
 
